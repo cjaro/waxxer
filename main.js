377fbc54800e0f2497c98afc82956c81c056346a
Catherine J <cjarocki@gmail.com>
2018-12-03 15:15:34 -0600

    working on temperatures

--- main.js
+++ main.js
@@ -1,41 +1,45 @@
-$(document).ready(function() {
-    var $fahrenheit = $('.option-fahrenheit');
-    var $celsius = $('.option-celsius')
-    var $kelvin = $('.option-kelvin');
-    var $olympics = $('.option-olypmics').hide();
-    var $dirt = $(.)
+function convertFtoC(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputCelsius").innerHTML = ((valNum - 32) / 1.8).toFixed(1);
+}
 
-    $('.wpp_search_select_field_location').change(function() {
-        var selectedValue = $(this).val();
+function convertCtoF(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputFahrenheit").innerHTML = ((valNum * 1.8) + 32).toFixed(1);
+}
 
-        if (selectedValue === 'Kelvin') {
-            $value1.show();
-            $value2.hide();
-        } else if (selectedValue === 'Costa Del Sol West') {
-            $value1.hide();
-            $value2.show();
-        } else {
-            $value1.hide();
-            $value2.hide();
-        }
-    });
-});
+// $(document).getElementById('temp-FC-select').addEventListener('change', function() {
+//     var style = this.value == 1 ? 'block' : 'none';
+//     document.getElementById('celsius-input').style.display = style;
+// });
 
 
+// Skelecode:
+// When tempInput is F, use convertFtoC()
+// When tempInput is C, use convertCtoF()
 
-// Convert from Fahrenheit to Celsius	℃=(℉-32)/1.8
-// <p>
-//   <label>Fahrenheit</label>
-//   <input id="inputFahrenheit" type="number" placeholder="Fahrenheit" oninput="temperatureConverter(this.value)" onchange="temperatureConverter(this.value)">
-// </p>
-// <p>Celcius: <span id="outputCelcius"></span></p>
-//
-// <script>
-// function temperatureConverter(valNum) {
-//   valNum = parseFloat(valNum);
-//   document.getElementById("outputCelcius").innerHTML=(valNum-32)/1.8;
-// }
-// </script>
+// $(document).ready(function() {
+//     var $fahrenheit = $('.option-fahrenheit');
+//     var $celsius = $('.option-celsius')
+//     var $kelvin = $('.option-kelvin');
+//     var $olympics = $('.option-olypmics').hide();
+//     var $dirt = $('.option-dirt');
+
+//     //     $('.wpp_search_select_field_location').change(function() {
+//     //         var selectedValue = $(this).val();
+
+//     //         if (selectedValue === 'Kelvin') {
+//     //             $value1.show();
+//     //             $value2.hide();
+//     //         } else if (selectedValue === 'Costa Del Sol West') {
+//     //             $value1.hide();
+//     //             $value2.show();
+//     //         } else {
+//     //             $value1.hide();
+//     //             $value2.hide();
+//     //         }
+//     //     });
+// });
 
 
 // Convert from Fahrenheit to Kelvin	K=((℉-32)/1.8)+273.15
@@ -53,19 +57,6 @@ $(document).ready(function() {
 // </script>
 
 
-// Convert from Celsius to Fahrenheit	℉=(℃*1.8)+32
-// <p>
-//   <label>Celsius</label>
-//   <input id="inputCelsius" type="number" placeholder="Celsius" oninput="temperatureConverter(this.value)" onchange="temperatureConverter(this.value)">
-// </p>
-// <p>Fahrenheit: <span id="outputFahrenheit"></span></p>
-//
-// <script>
-// function temperatureConverter(valNum) {
-//   valNum = parseFloat(valNum);
-//   document.getElementById("outputFahrenheit").innerHTML=(valNum*1.8)+32;
-// }
-// </script>
 
 
 // Convert from Celsius to Kelvin	K=℃+273.15
