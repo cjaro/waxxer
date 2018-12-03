6cf0d8f5d5161f34115412975b72eb1c4cee773d
Catherine J <cjarocki@gmail.com>
2018-12-03 16:22:46 -0600

    logic

--- main.js
+++ main.js
@@ -8,6 +8,37 @@ function convertCtoF(valNum) {
     document.getElementById("outputFahrenheit").innerHTML = ((valNum * 1.8) + 32).toFixed(1);
 }
 
+function returnWaxColor() {
+    var wax = document.getElementById('inputFahrenheit').value;
+    if (wax > 34 && wax < 70) {
+        document.getElementById("wax-color-div").innerHTML = "red";
+        console.log(wax, "red");
+    } else if (wax > 30 && wax < 33) {
+        document.getElementById("wax-color-div").innerHTML = "violet";
+        console.log(wax, "violet");
+    } else if (wax > 15 && wax < 29) {
+        document.getElementById("wax-color-div").innerHTML = "blue";
+        console.log(wax, "blue");
+    } else if (wax > -3 && wax < 14) {
+        document.getElementById("wax-color-div").innerHTML = "green";
+        console.log(wax, "green");
+    } else if (wax > -22 && wax < -4) {
+        document.getElementById("wax-color-div").innerHTML = "polar";
+        console.log(wax, "polar");
+    };
+    break returnWaxColor;
+}
+
+
+
+
+
+
+
+
+
+
+
 // $(document).getElementById('temp-FC-select').addEventListener('change', function() {
 //     var style = this.value == 1 ? 'block' : 'none';
 //     document.getElementById('celsius-input').style.display = style;
@@ -40,65 +71,3 @@ function convertCtoF(valNum) {
 //     //         }
 //     //     });
 // });
-
-
-// Convert from Fahrenheit to Kelvin	K=((℉-32)/1.8)+273.15
-// <p>
-//   <label>Fahrenheit</label>
-//   <input id="inputFahrenheit" type="number" placeholder="Fahrenheit" oninput="temperatureConverter(this.value)" onchange="temperatureConverter(this.value)">
-// </p>
-// <p>Kelvin: <span id="outputKelvin"></span></p>
-//
-// <script>
-// function temperatureConverter(valNum) {
-//   valNum = parseFloat(valNum);
-//   document.getElementById("outputKelvin").innerHTML=((valNum-32)/1.8)+273.15;
-// }
-// </script>
-
-
-
-
-// Convert from Celsius to Kelvin	K=℃+273.15
-// <p>
-//   <label>Celsius</label>
-//   <input id="inputCelsius" type="number" placeholder="Celsius" oninput="temperatureConverter(this.value)" onchange="temperatureConverter(this.value)">
-// </p>
-// <p>Kelvin: <span id="outputKelvin"></span></p>
-//
-// <script>
-// function temperatureConverter(valNum) {
-//   valNum = parseFloat(valNum);
-//   document.getElementById("outputKelvin").innerHTML=valNum+273.15;
-// }
-// </script>
-
-
-// Convert from Kelvin to Fahrenheit	℉=((K-273.15)*1.8)+32
-// <p>
-//   <label>Kelvin</label>
-//   <input id="inputKelvin" type="number" placeholder="Kelvin" oninput="temperatureConverter(this.value)" onchange="temperatureConverter(this.value)">
-// </p>
-// <p>Fahrenheit: <span id="outputFahrenheit"></span></p>
-//
-// <script>
-// function temperatureConverter(valNum) {
-//   valNum = parseFloat(valNum);
-//   document.getElementById("outputFahrenheit").innerHTML=((valNum-273.15)*1.8)+32;
-// }
-// </script>
-
-
-// Convert from Kelvin to Celsius	℃=K-273.15
-// <p>
-//   <label>Kelvin</label>
-//   <input id="inputKelvin" type="number" placeholder="Kelvin" oninput="temperatureConverter(this.value)" onchange="temperatureConverter(this.value)">
-// </p>
-// <p>Celcius: <span id="outputCelcius"></span></p>
-//
-// <script>
-// function temperatureConverter(valNum) {
-//   valNum = parseFloat(valNum);
-//   document.getElementById("outputCelcius").innerHTML=valNum-273.15;
-// }
-// </script>
