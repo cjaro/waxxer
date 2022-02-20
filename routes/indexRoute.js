ff0bcfc002de00ce1364fd48f7383d7cab0219da
Catherine Jarocki <cjarocki@gmail.com>
2022-02-20 00:22:53 -0600

    reframe title; add asset icons; json-ify icon translation info

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -3,8 +3,6 @@ const axios = require("axios");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const placeName = process.argv;
-
   res.render("index", {
     title: "☀️ 🌧 Query the weather, wherever you're heading ❄️ 🌩",
   });
@@ -16,6 +14,9 @@ router.post('/weather', async function (req, res) {
 
   const geoCodeUrl = constructGeoCodeUrl(req.body.placename);
   const geoCodeData = await queryAPI(geoCodeUrl);
+
+  const geoCodeState = geoCodeData.results[0].address_components[2].short_name;
+  const geocodeCounty = geoCodeData.results[0].address_components[1].long_name;
   const geoCodeLat = geoCodeData.results[0].geometry.location.lat;
   const geoCodeLng = geoCodeData.results[0].geometry.location.lng;
 
@@ -30,12 +31,12 @@ router.post('/weather', async function (req, res) {
     axios
       .get(openWeatherReqUrl)
       .then(result => {
-        let weatherJson = result.data;
-        let formattedWeather = formatWeatherData(result.data);
+        console.log(result.data);
+        let formattedWeather = formatWeatherData(result.data, geoCodeState, geocodeCounty);
 
         res.render("weather", {
           title: "☀️ 🌧 Weather! ❄️ 🌩",
-          weather: formattedWeather,
+          wx: formattedWeather,
         });
       })
       .catch(error => {
@@ -47,33 +48,52 @@ router.post('/weather', async function (req, res) {
 });
 
 
-function formatWeatherData(incomingJson) {
-  let newWeatherArray = [];
+function formatWeatherData(incomingJson, state, county) {
+  let weatherArray = {
+    "location": {},
+    "conditions": {},
+    "temps": {}
+  };
 
-  newWeatherArray["name"] = incomingJson.name;
-  newWeatherArray["country"] = incomingJson.sys.country;
-  newWeatherArray["latitude"] = incomingJson.coord.lat;
-  newWeatherArray["longitude"] = incomingJson.coord.lon;
-  newWeatherArray["condition"] = incomingJson.weather[0].main;
-  newWeatherArray["conditionDescription"] = incomingJson.weather[0].description;
-  newWeatherArray["currentTime"] = new Date(incomingJson.dt * 1000).toLocaleTimeString("en-US");
-  newWeatherArray["sunrise"] = new Date(incomingJson.sys.sunrise * 1000).toLocaleTimeString("en-US");
-  newWeatherArray["sunset"] = new Date(incomingJson.sys.sunset * 1000).toLocaleTimeString("en-US");
-  newWeatherArray["currentTempC"] = (incomingJson.main.temp - 273.15).toFixed(1);
-  newWeatherArray["currentTempF"] = ((incomingJson.main.temp - 273.15) * 9/5 + 32).toFixed(1);
-  newWeatherArray["tempHighC"] = (incomingJson.main.temp_max - 273.15).toFixed(1);
-  newWeatherArray["tempHighF"] = ((incomingJson.main.temp_max - 273.15) * 9/5 + 32).toFixed(1);
-  newWeatherArray["tempLowC"] = (incomingJson.main.temp_min - 273.15).toFixed(1);
-  newWeatherArray["tempLowF"] = ((incomingJson.main.temp_min - 273.15) * 9/5 + 32).toFixed(1);
-  newWeatherArray["feelsLikeC"] = (incomingJson.main.feels_like - 273.15).toFixed(1);
-  newWeatherArray["feelsLikeF"] = ((incomingJson.main.feels_like - 273.15) * 9/5 + 32).toFixed(1);
-  newWeatherArray["humidity"] = incomingJson.main.humidity;
-  newWeatherArray["pressure"] = incomingJson.main.pressure;
-  newWeatherArray["windSpeed"] = incomingJson.wind.speed;
-  newWeatherArray["cloudCover"] = incomingJson.clouds.all;
-  newWeatherArray["iconCode"] = incomingJson.weather[0].icon;
+  weatherArray["location"]["name"] = incomingJson.name;
+  weatherArray["location"]["state"] = state;
+  weatherArray["location"]["county"] = county;
+  weatherArray["location"]["country"] = incomingJson.sys.country;
+  weatherArray["location"]["latitude"] = incomingJson.coord.lat;
+  weatherArray["location"]["longitude"] = incomingJson.coord.lon;
 
-  return newWeatherArray;
+  weatherArray["conditions"]["openWeatherMapId"] = incomingJson.weather[0].id;
+  weatherArray["conditions"]["condition"] = incomingJson.weather[0].main;
+  weatherArray["conditions"]["conditionDescription"] = incomingJson.weather[0].description;
+  weatherArray["conditions"]["iconCode"] = incomingJson.weather[0].icon;
+
+  weatherArray["temps"]["currentTempC"] = (incomingJson.main.temp - 273.15).toFixed(1);
+  weatherArray["temps"]["currentTempF"] = ((incomingJson.main.temp - 273.15) * 9/5 + 32).toFixed(1);
+  weatherArray["temps"]["tempHighC"] = (incomingJson.main.temp_max - 273.15).toFixed(1);
+  weatherArray["temps"]["tempHighF"] = ((incomingJson.main.temp_max - 273.15) * 9/5 + 32).toFixed(1);
+  weatherArray["temps"]["tempLowC"] = (incomingJson.main.temp_min - 273.15).toFixed(1);
+  weatherArray["temps"]["tempLowF"] = ((incomingJson.main.temp_min - 273.15) * 9/5 + 32).toFixed(1);
+  weatherArray["temps"]["feelsLikeC"] = (incomingJson.main.feels_like - 273.15).toFixed(1);
+  weatherArray["temps"]["feelsLikeF"] = ((incomingJson.main.feels_like - 273.15) * 9/5 + 32).toFixed(1);
+
+  weatherArray["currentTime"] = new Date(incomingJson.dt * 1000).toLocaleTimeString("en-US");
+  weatherArray["sunrise"] = new Date(incomingJson.sys.sunrise * 1000).toLocaleTimeString("en-US");
+  weatherArray["sunset"] = new Date(incomingJson.sys.sunset * 1000).toLocaleTimeString("en-US");
+
+  weatherArray["humidity"] = incomingJson.main.humidity;
+  weatherArray["pressure"] = incomingJson.main.pressure;
+  weatherArray["windSpeed"] = incomingJson.wind.speed;
+  weatherArray["cloudCover"] = incomingJson.clouds.all;
+
+  console.log(weatherArray);
+
+  return weatherArray;
+}
+
+function changeWeatherCode(iconCode) {}
+
+function recommendWax(tempCelsius, humidity, condition, conditionDescription) {
+  // cobble together a wax recommendation, given temperature, humidity, snowfall, and general conditions
 }
 
 function constructGeoCodeUrl(placeName) {
