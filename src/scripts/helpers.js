163887bb52a54b3cb7701e5c74e6dc6ea6a86916
C Jarocki <cjarocki@gmail.com>
2022-03-06 22:55:56 -0600

    oops wind degrees actually work now

--- src/scripts/helpers.js
+++ src/scripts/helpers.js
@@ -95,39 +95,39 @@ module.exports = {
   // http://snowfence.umn.edu/Components/winddirectionanddegrees.htm
   // Looks horrific but someone else did the math: https://stackoverflow.com/questions/6665997/
   interpretWindDegrees: function(degrees) {
-    if (348.75 <= degrees < 360) {
+    if (degrees < 360 && degrees >= 348.75) {
       return "N";
-    } else if (0 <= degrees < 11.25) {
+    } else if (degrees < 11.25 && degrees >= 0) {
       return "N";
-    } else if (11.25 <= degrees < 33.75) {
+    } else if (degrees < 33.75 && degrees >= 11.25) {
       return "NNE";
-    } else if (33.75 <= degrees < 56.25) {
+    } else if (degrees < 56.25 && degrees >= 33.75) {
       return "NE";
-    } else if (56.25 <= degrees < 78.75) {
+    } else if (degrees < 78.75 && degrees >= 56.25) {
       return "ENE";
-    } else if (78.75 <= degrees < 101.25) {
+    } else if (degrees < 101.25 && degrees >= 78.75) {
       return "E";
-    } else if (101.25 <= degrees < 123.75) {
+    } else if (degrees < 123.75 && degrees >= 101.25) {
       return "ESE";
-    } else if (123.75 <= degrees < 146.25) {
+    } else if (degrees < 146.25 && degrees >= 123.75) {
       return "SE";
-    } else if (146.25 <= degrees < 168.75) {
+    } else if (degrees < 168.75 && degrees >= 146.25) {
       return "SSE";
-    } else if (168.75 <= degrees < 191.25) {
+    } else if (degrees < 191.25 && degrees >= 168.75) {
       return "S";
-    } else if (191.25 <= degrees < 213.75) {
+    } else if (degrees < 213.75 && degrees >= 191.25) {
       return "SSW";
-    } else if (213.75 <= degrees < 236.25) {
+    } else if (degrees < 236.25 && degrees >= 213.75) {
       return "SW";
-    } else if (236.25 <= degrees < 258.75) {
+    } else if (degrees < 258.75 && degrees >= 236.25) {
       return "WSW";
-    } else if (258.75 <= degrees < 281.25) {
+    } else if (degrees < 281.25 && degrees >= 258.75) {
       return "W";
-    } else if (281.25 <= degrees < 303.75) {
+    } else if (degrees < 303.75 && degrees >= 281.25) {
       return "WNW";
-    } else if (303.75 <= degrees < 326.25) {
+    } else if (degrees < 326.25 && degrees >= 303.75) {
       return "NW";
-    } else if (326.25 <= degrees < 348.75) {
+    } else if (degrees < 348.75 && degrees >= 326.25) {
       return "NNW";
     }
   },
